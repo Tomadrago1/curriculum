47792581ea8c43a0124483c4fa8db6928f49487a
Tomadrago1 <126613478+Tomadrago1@users.noreply.github.com>
2023-02-28 16:48:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function evento(){
+    alert('Por razones de seguridad no puedo proporcionar mi wpp')
+}
+
+const buttons = document.querySelectorAll('a');
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(e){
+
+        let x=e.clientX - e.target.offsetLeft;
+        let y=e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples)
+
+        setTimeout(() =>{
+            ripples.remove()
+        },1000)
+    })
+})
